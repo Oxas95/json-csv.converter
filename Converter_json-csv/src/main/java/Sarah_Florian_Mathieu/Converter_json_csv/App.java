5541f783c084c3fbd557865bf2bf03c3bac654eb
Florian Ldm <ledeme.florian@gmail.com>
2019-12-14 17:04:57 +0100

    maj app.java avexc nouvelle arborescence du projet

--- Converter_json-csv/src/main/java/Sarah_Florian_Mathieu/Converter_json_csv/App.java
+++ Converter_json-csv/src/main/java/Sarah_Florian_Mathieu/Converter_json_csv/App.java
@@ -1,19 +1,18 @@
 package Sarah_Florian_Mathieu.Converter_json_csv;
 
 import java.io.IOException;
-import java.util.IllegalFormatCodePointException;
-import java.util.IllegalFormatException;
 import java.util.Scanner;
 
-import Sarah_Florian_Mathieu.Converter_json_csv.converter.CsvManager;
-import Sarah_Florian_Mathieu.Converter_json_csv.converter.JsonManager;
+import Sarah_Florian_Mathieu.Converter_json_csv.converter.manager.CsvException;
+import Sarah_Florian_Mathieu.Converter_json_csv.converter.manager.CsvManager;
+import Sarah_Florian_Mathieu.Converter_json_csv.converter.manager.JsonManager;
 
 /**
- * 
+ *
  * @author user
  *
  */
-public enum App 
+public enum App
 {
 	APPLICATION;
 
@@ -24,8 +23,11 @@ public enum App
 	 * @param fic_out
 	 * @throws FileFormatException if file format isn't csv or json
 	 * @throws IOException
+	 * @throws CsvException
+	 * @throws IllegalArgumentException
+	 * @throws NullPointerException
 	 */
-	public static void launchApp(String fic_in, String fic_out) throws FileFormatException, IOException {
+	public static void launchApp(String fic_in, String fic_out) throws FileFormatException, IOException, NullPointerException, IllegalArgumentException, CsvException {
 		if(fic_in.endsWith(".csv")){
 			CsvManager cm = new CsvManager(fic_in);
 			JsonManager.parseJsonFile(fic_out, cm.getArrayCopy(), cm.getWidth(), cm.getHeight());
@@ -41,8 +43,11 @@ public enum App
 	 * Method which asks an input file and a name for the output file and calls method launchApp()
 	 * @throws FileFormatException
 	 * @throws IOException
+	 * @throws CsvException
+	 * @throws IllegalArgumentException
+	 * @throws NullPointerException
 	 */
-	public static void interact() throws FileFormatException, IOException{
+	public static void interact() throws FileFormatException, IOException, NullPointerException, IllegalArgumentException, CsvException{
 		System.out.println("******CSV/JSON CONVERTER******");
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Veuillez saisir un fichier d'entrée :");
@@ -59,20 +64,23 @@ public enum App
 
 		launchApp(fic_in,fic_out);
 	}
-	
+
 	/**
-	 * 
+	 *
 	 * @param args not used
 	 * @throws IOException if problem to read a file used by the converter
+	 * @throws CsvException
+	 * @throws IllegalArgumentException
+	 * @throws NullPointerException
 	 */
-    public static void main( String[] args ) throws IOException, FileFormatException {
-    	//interact();
-    	//exemple csv->json->csv->csv
+	public static void main( String[] args ) throws IOException, FileFormatException, NullPointerException, IllegalArgumentException, CsvException {
+		//interact();
+		//exemple csv->json->csv->csv
 		CsvManager cm = new CsvManager("exemple.csv");
-    	JsonManager.parseJsonFile("csv->json", cm.getArrayCopy(), cm.getWidth(), cm.getHeight());
-    	JsonManager jm = new JsonManager("csv->json.json");
-    	CsvManager.parseCsvFile("csv->json->csv", jm.getArrayCopy(), jm.getWidth(), jm.getHeight());
-    	CsvManager cm2 = new CsvManager("csv->json->csv.csv");
-    	CsvManager.parseCsvFile("csv->json->csv->csv", cm2.getArrayCopy(), cm2.getWidth(), cm2.getHeight());
-    }
+		JsonManager.parseJsonFile("csv->json", cm.getArrayCopy(), cm.getWidth(), cm.getHeight());
+		JsonManager jm = new JsonManager("csv->json.json");
+		CsvManager.parseCsvFile("csv->json->csv", jm.getArrayCopy(), jm.getWidth(), jm.getHeight());
+		CsvManager cm2 = new CsvManager("csv->json->csv.csv");
+		CsvManager.parseCsvFile("csv->json->csv->csv", cm2.getArrayCopy(), cm2.getWidth(), cm2.getHeight());
+	}
 }
